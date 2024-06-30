26da28ddeab994b38ed1838fc16cac10b89a2e78
dsvallala <sumedhanakka98@gmail.com>
2024-06-30 01:55:58 -0400

    forms

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-  standalone: true,
-  imports: [],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-
+  onSubmit() {
+    console.log('Form Submitted!');
+  }
 }
