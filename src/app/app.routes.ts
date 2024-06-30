e2d1fc8829b8b9a0e5f68256cd008c0c2e4072a5
dsvallala <sumedhanakka98@gmail.com>
2024-06-30 01:18:12 -0400

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactComponent } from './contact/contact.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'admin', component: AdminComponent },
+];
